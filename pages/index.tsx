ed89a166a089b05cb43b87e8b6fefbbb73282b0d
greatSumini <greatsumini@gmail.com>
2020-11-09 00:31:52 +0900

    feat: :package: set babel, styled-components

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Home() {
+  return <Wrapper>안녕 나는 수민</Wrapper>;
+}
+
+const Wrapper = styled.div`
+  width: 500px;
+  height: 500px;
+  background-color: red;
+`;
